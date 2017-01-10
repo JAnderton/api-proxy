7f9428afa09938476a24e22da87b45e20165c8ac
Karun Japhet <karun@japhet.in>
2017-01-10 17:45:03 -0500

    Fix request script for quicker testing

--- request.sh
+++ request.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl -s --header "content-type: text/xml" -d @proxy/src/test/resources/request.xml http://localhost:8080/proxy | xmllint --format -
+curl -s --header "content-type: text/xml" -d @proxy/src/test/resources/request.xml http://localhost:8080/proxy
 curl -s localhost:8000/proxy/metrics
